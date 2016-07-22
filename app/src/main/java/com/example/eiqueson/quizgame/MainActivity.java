af8284b9152f87feaff758cd56bfb7ca552a9d8e
Wannasak Rungpörnprasit <Wannasak Rungprnprasit>
2016-07-22 15:40:06 +0700

    Add exit dialog when press a back button

--- app/src/main/java/com/example/eiqueson/quizgame/MainActivity.java
+++ app/src/main/java/com/example/eiqueson/quizgame/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.eiqueson.quizgame;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBackPressed()
+    {
+        final AlertDialog.Builder exitDialog = new AlertDialog.Builder(this);
+        exitDialog.setTitle("Exit");
+        exitDialog.setMessage("Do you want to quit ?");
+        exitDialog.setCancelable(true);
+        exitDialog.setIcon(R.drawable.meme15);
+        exitDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        exitDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        exitDialog.show();
     }
 }
